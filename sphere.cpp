b9d911e7941676dc5a963a070b1433b8a17ad664
Ubuntu <ubuntu@ip-172-31-80-220.ec2.internal>
2022-04-01 01:53:22 +0000

    finished sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -11,11 +11,13 @@
 
 // declare variables
 float radius, surfaceArea, volume;
-std::string units;
+
 
 
 int main() {
-    // input
+    // declare string variable in the function
+    std::string units;
+    // get the radius and units to calculate for the surface area and volume
     std::cout << "Today we will calculate the surface area and";
     std::cout << " volume of a sphere\n";
     std::cout << "\n";
@@ -24,13 +26,14 @@ int main() {
     std::cout << "What is the radius of your sphere? (" << units << "): ";
     std::cin >> radius;
 
-    // process
+    // calculate the surface area and volume of the sphere
     surfaceArea = 4 * M_PI * pow(radius, 2);
     volume =  M_PI * pow(radius, 3) * 4/3;
 
-    // output
+    // display the surface area and volume of the sphere
     std::cout << "\n";
     std::cout << std::endl;
-    std::cout << "The Surface Area of the sphere is: " << surfaceArea << units << "^2\n";
+    std::cout << "The Surface Area of the sphere is: ";
+    std::cout << surfaceArea << units << "^2\n";
     std::cout << "The volume is: " << volume << units << "^3\n";
 }
